c287904718b56a70b54029cac511b5030d27f591
Andrei <97633022+Zmei55@users.noreply.github.com>
2024-11-18 11:53:46 +0100

    (competa-2)_Input_updated

--- shared/ui/Input.tsx
+++ shared/ui/Input.tsx
@@ -1,22 +1,45 @@
+import { useState } from 'react';
+import { Pressable, StyleSheet, View } from 'react-native';
 import { TextInput, TextInputProps } from 'react-native-paper';
-import { Colors } from 'shared/theme';
+import EyeClosedIcon from 'shared/icons/EyeClosed';
+import EyeOpeningIcon from 'shared/icons/EyeOpening';
+import { Colors, Theme } from 'shared/theme';
 
 type InputProps = TextInputProps & {
-  password?: boolean;
+  isPassword?: boolean;
 };
 
 
-export const Input: React.FC<InputProps> = ({ password = false, ...rest }) => {
+export const Input: React.FC<InputProps> = ({ isPassword = false, ...rest }) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+
   return (
-    <TextInput
-      style={{
-        height: 50,
-      }}
-      mode='outlined'
-      outlineColor={Colors.primary}
-      activeOutlineColor={Colors.primaryDark}
-      secureTextEntry={password}
-      {...rest}
-    />
+    <View>
+      <TextInput
+        style={{
+          height: 50,
+        }}
+        mode='outlined'
+        outlineColor={Colors.primary}
+        activeOutlineColor={Colors.primaryDark}
+        secureTextEntry={isPassword && !isPasswordVisible}
+        {...rest}
+      />
+      {isPassword && (
+        <Pressable onPress={() => setIsPasswordVisible(state => !state)} style={styles.eyeIcon}>
+          {isPasswordVisible ? <EyeOpeningIcon /> : <EyeClosedIcon />}
+        </Pressable>
+      )}
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  eyeIcon: {
+    position: 'absolute',
+    top: Theme.spacing(5),
+    right: Theme.spacing(3),
+    height: Theme.spacing(6),
+    width: Theme.spacing(6),
+  },
+})
