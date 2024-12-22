294441d30899ede472c106a528b184f9a9e63c2e
Andrei <97633022+Zmei55@users.noreply.github.com>
2024-12-22 21:38:09 +0100

    (competence-4)_Spinner_updated

--- shared/ui/Spinner.tsx
+++ shared/ui/Spinner.tsx
@@ -9,11 +9,6 @@ interface SpinnerProps extends ActivityIndicatorProps {
   color?: SpinnerColorType;
 }
 
-const spinnerColor = (color: SpinnerColorType) => {
-  if (color === 'white') return Colors.white;
-  return Colors.primary;
-};
-
 const spinnerSize = (size: SpinnerSizeType) => {
   if (size === 'small') return 25;
   if (size === 'large') return 50;
@@ -29,7 +24,7 @@ export const Spinner: React.FC<SpinnerProps> = ({
   return (
     <ActivityIndicator
       animating={true}
-      color={spinnerColor(color)}
+      color={Colors[color]}
       {...rest}
       size={spinnerSize(size)}
     />
